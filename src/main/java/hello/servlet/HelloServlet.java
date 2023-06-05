effc8640ca7479e4a6344b4cc2ec5a686e0fe482
KDH_Notebook <destiny1017@nate.com>
2023-06-05 21:32:43 +0900

    서블릿 컨테이너와 스프링 컨테이너

---
+++ src/main/java/hello/servlet/HelloServlet.java
@@ -0,0 +1,17 @@
+package hello.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.http.HttpServlet;
+
+import java.io.IOException;
+
+
+public class HelloServlet extends HttpServlet {
+    @Override
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+        System.out.println("HelloServlet.service");
+        res.getWriter().write("hello servlet!");
+    }
+}
--- src/main/java/hello/servlet/TestServlet.java
+++ src/main/java/hello/servlet/TestServlet.java
@@ -1,4 +1,4 @@
-package hello;
+package hello.servlet;
 
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -12,9 +12,10 @@ import java.io.IOException;
  */
 @WebServlet(urlPatterns = "/test")
 public class TestServlet extends HttpServlet {
+
     @Override
     protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        System.out.println("TestServlet.service");
+        System.out.println("TestServlet.service!");
         resp.getWriter().println("test");
     }
 }
